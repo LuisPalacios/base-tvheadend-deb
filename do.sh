a3077605865bbedd0cc54c391a6523a86901c1aa
Luis Palacios <luis@luispa.com>
2015-05-01 10:15:14 +0200

    Modificaciones para trabajar en modo debug

--- do.sh
+++ do.sh
@@ -13,15 +13,19 @@
 #
 ##################################################################
 
+# Si "no" estamos en MODO DEBUG y además existe el .DEB entonces lo
+# copio al directorio /output
 #
-if [ -f /srv/tvheadend_3.9*precise_amd64.deb ]; then
+if [ -z "${DEBUG}" ];then 	
+    if [ -f /srv/tvheadend_3.9*precise_amd64.deb ]; then
 
-	src=`ls -1 /srv/tvheadend_3*.deb`
-	echo "Copio ${src} al directorio actual."
-	cp ${src} /output
+	    src=`ls -1 /srv/tvheadend_3*.deb`
+    	echo "Copio ${src} al directorio actual."
+	    cp ${src} /output
 
-    exit
-fi
+        exit
+    fi
+fi    
 
 # En caso de no encontrar el fichero ejecuto el comando
 # que me piden (debería ser /bin/bash para hacer debug)
--- tvheadend-build-deb.sh
+++ tvheadend-build-deb.sh
@@ -5,4 +5,11 @@
 # Script que extrae el tvheadend_3.9.XXXX~yyyyyyyy~precise_amd64.deb para poder
 # ser usado en el proyecto luispa/base-tvheadend
 #
-docker run -t -i -v $(pwd):/output luispa/base-tvheadend-deb /bin/bash
+#docker run -t -i -v $(pwd):/output luispa/base-tvheadend-deb /bin/bash
+
+
+#
+# Para ejecutar en modo debug, comentar la línea anterior y descomentar la siguiente:
+#
+docker run -t -i -e DEBUG="yes" -v $(pwd):/output luispa/base-tvheadend-deb /bin/bash
+
